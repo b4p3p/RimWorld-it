ecdcbbba501c6284df05b9e07738a13e7bc3d710
b4p3p <montrone.giuseppe@gmail.com>
2025-07-05 21:18:11 +0200

    fix install script. added keys

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -8,12 +8,12 @@
 
 $basePath = 'C:\Users\montr\progetti\RimWorld-it'
 
-# copia i file originali in inglese
-robocopy 'C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Data\Core\Languages\English' "$basePath\English\Core" /NFL /NDL /E /XO /XN /NJH /NJS /NC /NS /NP
-robocopy 'C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Data\Royalty\Languages\English' "$basePath\English\Royalty" /NFL /NDL /E /XO /XN /NJH /NJS /NC /NS /NP
-robocopy 'C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Data\Ideology\Languages\English' "$basePath\English\Ideology" /NFL /NDL /E /XO /XN /NJH /NJS /NC /NS /NP
-robocopy 'C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Data\Biotech\Languages\English' "$basePath\English\Biotech" /NFL /NDL /E /XO /XN /NJH /NJS /NC /NS /NP
-robocopy 'C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Data\Anomaly\Languages\English' "$basePath\English\Anomaly" /NFL /NDL /E /XO /XN /NJH /NJS /NC /NS /NP
+# copia i file originali dall'inglese inglese
+robocopy 'C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Data\Core\Languages\English' "$basePath\English\Core"  /MIR /NFL /NDL /E /XO /XN /NJH /NJS /NC /NS /NP
+robocopy 'C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Data\Royalty' "$basePath\English\Royalty"  /MIR /NFL /NDL /E /XO /XN /NJH /NJS /NC /NS /NP
+robocopy 'C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Data\Ideology\Languages\English' "$basePath\English\Ideology"  /MIR /NFL /NDL /E /XO /XN /NJH /NJS /NC /NS /NP
+robocopy 'C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Data\Biotech\Languages\English' "$basePath\English\Biotech"  /MIR /NFL /NDL /E /XO /XN /NJH /NJS /NC /NS /NP
+robocopy 'C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Data\Anomaly\Languages\English' "$basePath\English\Anomaly"  /MIR /NFL /NDL /E /XO /XN /NJH /NJS /NC /NS /NP
 
 # installa la patch
 robocopy "$basePath\Core" 'C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Data\Core\Languages\Italiano' /NFL /NDL /MIR /NJH /NJS /NC /NS /NP
